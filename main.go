4a0901e97c612c79beb5eaeb975455c29ed98182
baeklee <77774331+yahoo557@users.noreply.github.com>
2025-03-20 17:53:54 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/yahoo557/gin-boilerplate/internal/user"
 	"time"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Logger() gin.HandlerFunc {
@@ -21,41 +19,9 @@ func main() {
 	r := gin.Default()
 	r.Use(Logger())
 
-	/*
-	 * PreRequisite: Prometheus
-	 * **************************** */
-	collector := plugins.InitializePrometheusCollector(plugins.PrometheusCollectorConfig{
-		Namespace: conf.GetString("app.name"),
-	})
-	http.Handle(conf.GetString("prometheus.route"), promhttp.Handler())
+	apiV1 := r.Group("/api/v1")
 
-	/*
-	 * PreRequisite: Hystrix
-	 * **************************** */
-	// Expose CB Prometheus metrics
-	metricCollector.Registry.Register(collector.NewPrometheusCollector)
-
-	/*
-	 * PreRequisite: Health Check + Expose status Prometheus metrics gauge
-	 * **************************** */
-	healthChecker := healthcheck.NewMetricsHandler(prometheus.DefaultRegisterer, "health_check")
-	healthChecker.AddLivenessCheck("Goroutine Threshold", healthcheck.GoroutineCountCheck(conf.GetInt("health_check.goroutine_threshold")))
-
-	// Expose to HTTP
-	http.HandleFunc(conf.GetString("health_check.route.group")+conf.GetString("health_check.route.live"), healthChecker.LiveEndpoint)
-	http.HandleFunc(conf.GetString("health_check.route.group")+conf.GetString("health_check.route.ready"), healthChecker.ReadyEndpoint)
-	
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "Server listening on 8080",
-		})
-	})
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	user.UserRouter(apiV1)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
